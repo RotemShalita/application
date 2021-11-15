8485b1377e63c7a626f91e2d54851a322c76a2d8
Rotem Shalita <rotem1shalita@gmail.com>
2021-11-15 14:50:10 +0200

    adding templates folder and other modifications

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import json
+import os
 
 app = Flask(__name__)
 
+def get_jsons():
+    students_dir = "..\\wis-advanced-python-2021-2022\\students"
+    return [(json.load(open(os.path.join(students_dir, i))), os.path.join(students_dir, i)) for i in os.listdir(students_dir)]
+
 @app.route("/")
 def main():
-    return "Hello World! Rotem"
+    names = []
+    for student, path in get_jsons():
+        names += [(student["name"], path)]
+
+    print(names)
+    return render_template('app.html', names=names)
+
+@app.route("/search", methods=['POST'])
+def search():
+    if not 'text' in request.form:
+        return "Bad search value"
+    searched_str = request.form['text'].lower()
+
+    searched_list = []
+    for student in get_jsons():
+        if searched_str in student["name"].lower():
+            searched_list += [student["name"]]
+
+    return render_template('app.html', names=searched_list)
+
+    
