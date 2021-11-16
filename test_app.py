1627a2d478318e17fec1eb23232d4ab773a566b8
Rotem Shalita <rotem1shalita@gmail.com>
2021-11-16 09:50:20 +0200

    modified the tests and README.md

--- test_app.py
+++ test_app.py
@@ -5,19 +5,7 @@ def test_app():
 
     rv = web.get('/')
     assert rv.status == '200 OK'
-    assert '<form action="/echo" method="POST">' in rv.data.decode('utf-8')
+    assert '<form action="/search" method="POST">' in rv.data.decode('utf-8')
 
 
-    rv = web.get('/echo')
-    assert rv.status == '405 METHOD NOT ALLOWED'
-    assert '<title>405 Method Not Allowed</title>' in rv.data.decode('utf-8')
 
-
-    rv = web.post('/echo')
-    assert rv.status == '200 OK'
-    assert b"Nothing to say?" == rv.data
-
-
-    rv = web.post('/echo', data={ "text": "foo bar" })
-    assert rv.status == '200 OK'
-    assert b"You said: foo bar" == rv.data
